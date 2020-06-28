b4b6db66561535c28207ae4198cdef2cc44064f4
grelad <gerchenenko@gmail.com>
2020-06-28 13:51:07 +0300

    Add menu-item, directory components and styles for it. Move homepage component to pages

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import './homepage.styles.scss'
+
+import Directory from '../../components/directory/directory.component';
+import React from 'react';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+)
+
+export default Homepage;
