5377306f5c88d6cc1e193667c755c12633dcdb6b
grelad <gerchenenko@gmail.com>
2020-07-01 23:41:21 +0300

    Add shop page, shop, collection-item, collection-preview components. Add images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 
+import { Route, Switch } from 'react-router-dom';
+
 import Homepage from './pages/homepage/homepage.component'
 import React from 'react';
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
-      <Homepage></Homepage>
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
